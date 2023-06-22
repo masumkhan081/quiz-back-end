fc53e15c295d54dcddd62fa4344ed5e53ff7b54a
masum khan <study.contents.of.masumkhan@gmail.com>
2023-06-22 09:28:11 +0600

    dashboard summery ipdated....

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,39 +5,57 @@ dotenv.config();
 const { pool, client } = require("../database/connection");
 
 dashboardRoute.get("/dashboard", (req, res) => {
-  let objResult = {};
+  let obj_result = {};
   pool.query(`SELECT COUNT(*) FROM quizes `, (err, result) => {
     if (result) {
-      let data = result.rows;
-      objResult.total_quiz = data;
+      obj_result.num_quiz = result.rows[0].count;
       pool.query(
         `SELECT COUNT(*) FROM quizes WHERE charge = 0 `,
         (err, result) => {
           if (result) {
-            let data2 = result.rows;
-            objResult.free_quiz = data2;
+            obj_result.num_free_quiz = result.rows[0].count;
             pool.query(
               `SELECT COUNT(*) FROM quizes WHERE charge > 0 `,
               (err, result) => {
                 if (result) {
-                  let data3 = result.rows;
-                  objResult.paid_quiz = data3;
-                  res.json({ summary: objResult });
+                  obj_result.num_paid_quiz = result.rows[0].count;
+
+                  pool.query(`SELECT COUNT(*) FROM users `, (err, result) => {
+                    if (result) {
+                      obj_result.num_user = result.rows[0].count;
+                      res.json(obj_result);
+                      // pool.query(
+                      //   `SELECT COUNT(*) FROM users `,
+                      //   (err, result) => {
+                      //     if (result) {
+                      //       obj_result.num_users = result.rows[0].count;
+                      //       res.json(obj_result);
+                      //     }
+                      //     if (err) {
+                      //       res.send("error - 4" + err);
+                      //     }
+                      //   }
+                      // );
+                    }
+                    if (err) {
+                      res.send("error - 4" + err);
+                    }
+                  });
                 }
                 if (err) {
-                  res.send("error -3" + err);
+                  res.send("error - 3" + err);
                 }
               }
             );
           }
           if (err) {
-            res.send("error -2 " + err);
+            res.send("error - 2 " + err);
           }
         }
       );
     }
     if (err) {
-      res.send("error -1 " + err);
+      res.send("error - 1 " + err);
     }
   });
   pool.end;
